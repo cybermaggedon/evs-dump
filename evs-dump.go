5296554b42481976ae8c5e07cca7acb1dd97e778
Mark Adams <cybermaggedon@gmail.com>
2020-06-18 13:36:42 +0100

    Working with new API

--- evs-dump.go
+++ evs-dump.go
@@ -4,47 +4,76 @@ import (
 	"encoding/json"
 	"fmt"
 	evs "github.com/cybermaggedon/evs-golang-api"
-	"os"
-	"strings"
+	"log"
 )
+
+type DumpConfig struct {
+	*evs.Config
+}
+
+func NewDumpConfig() *DumpConfig {
+	return &DumpConfig{
+		Config: evs.NewConfig("cyberprobe"),
+	}
+}
 
 // Dump analytic, just dumps out a kinda JSON equivalent of the event message.
 type Dump struct {
-	evs.EventAnalytic
+	*DumpConfig
+	*evs.EventSubscriber
+	*evs.EventProducer
+	evs.Interruptible
 }
+
+func NewDump(dc *DumpConfig) *Dump {
+
+	d := &Dump{ DumpConfig: dc }
+
+	var err error
+	d.EventSubscriber, err = evs.NewEventSubscriber("evs-dump", d.Input, d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d.EventProducer, err = evs.NewEventProducer(d.Outputs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d.RegisterStop(d)
+
+	return d
+}
+	
 
 // Event handler
 func (d *Dump) Event(ev *evs.Event, properties map[string]string) error {
 
-	// Marshal as JSON.  This doesn't work well, I couldn't get protojson to work on
-	// these messages.
+	// Marshal as JSON.  This doesn't work well, I couldn't get protojson
+	// to work on these messages.
+
 	j, err := json.Marshal(ev)
 	if err != nil {
 		return err
 	}
+
 	fmt.Println(string(j))
-	d.OutputEvent(ev, properties)
+
+	d.Output(ev, properties)
+
 	return nil
+
 }
 
 func main() {
 
-	d := &Dump{}
+	dc := NewDumpConfig()
 
-	binding, ok := os.LookupEnv("INPUT")
-	if !ok {
-		binding = "cyberprobe"
-	}
-
-	out, ok := os.LookupEnv("OUTPUT")
-	if !ok {
-		d.Init(binding, []string{}, d)
-	} else {
-		outarray := strings.Split(out, ",")
-		d.Init(binding, outarray, d)
-	}
-
+	d := NewDump(dc)
+	
 	d.Run()
+
+	log.Print("Shutdown.")
 
 }
 
